cee9bc9612f5e0abf11fb643fd01a2b38bd82403
Nguyen Van Hung <hungnv@Hungs-Mac.local>
2019-04-04 18:05:47 +0700

    add justified layout demo

---
+++ justifiedLayoutDemo/app.js
@@ -0,0 +1,83 @@
+const express = require('express')
+const sizeOf = require('image-size');
+const justifiedLayout = require('justified-layout')
+const app = express()
+const port = 3000
+app.use(express.static('public'))
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+
+
+app.get('/', (req, res) => {
+    // get images size
+    let responseArr = []
+    let image, dimensions
+    for (let i = 1; i <= 20; i++) {
+      image = 'public/image/' + i + '.png'
+      dimensions = sizeOf(image)
+      responseArr.push(
+        {
+          width: dimensions.width,
+          height: dimensions.height,
+          url: image
+        }
+      )
+    }
+    responseArr = shuffle(responseArr)
+
+    // build layout
+    var geometry = justifiedLayout(responseArr)
+    console.log("geometry", geometry)
+    
+    var data = ''
+    for (let j = 0; j < geometry.boxes.length; j++) {
+        let box = geometry.boxes[j]
+        data += `<div class="box" style="width: ${box.width}px; height: ${box.height}px; top: ${box.top}px; left: ${box.left}px"> <img src="${responseArr[j].url.slice(6)}"> </div> \n`
+    }
+
+
+
+    var section = `<div class="justified">${data}</div>`
+
+    section +=`    <style type="text/css" media="screen">
+                *{
+                    box-sizing: border-box;
+                }
+                .justified {
+                    position: relative;
+                    background: seagreen;
+                    width: 1060px;
+                    margin: auto;
+                }
+
+                .box {
+                    position: absolute;
+                    overflow: hidden;
+                    border: 1px solid;
+                }
+                img{
+                    max-width: 100%;
+                    max-height: 100%;
+                }
+            </style>`
+    res.send(section)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
