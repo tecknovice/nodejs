f8364b8cbf5b2ddf2737ae4222cabf6dc25b6777
tecknovice <tecknovice@gmail.com>
2020-01-14 11:44:11 +0700

    add lb3-relations project

---
+++ lb3-relations/server/server.js
@@ -0,0 +1,41 @@
+// Copyright IBM Corp. 2016. All Rights Reserved.
+// Node module: loopback-workspace
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+'use strict';
+
+var loopback = require('loopback');
+var boot = require('loopback-boot');
+var path = require('path');
+
+var app = module.exports = loopback();
+app.set('view engine', 'ejs'); // LoopBack comes with EJS out-of-box
+app.set('json spaces', 2); // format json responses for easier viewing
+
+// must be set to serve views properly when starting the app via `slc run` from
+// the project root
+app.set('views', path.resolve(__dirname, 'views'));
+
+app.start = function() {
+  // start the web server
+  return app.listen(function() {
+    app.emit('started');
+    const baseUrl = app.get('url').replace(/\/$/, '');
+    console.log('Web server listening at: %s', baseUrl);
+    if (app.get('loopback-component-explorer')) {
+      const explorerPath = app.get('loopback-component-explorer').mountPath;
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    }
+  });
+};
+
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+boot(app, __dirname, function(err) {
+  if (err) throw err;
+
+  // start the server if `$ node server.js`
+  if (require.main === module)
+    app.start();
+});
